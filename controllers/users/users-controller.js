3e68c2d2c522b4ce0b979ea59f44392ac8e00305
zjw981119 <962610351@qq.com>
2022-12-12 00:06:38 -0500

    Implement find all users without logged-in user api

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -1,56 +1,33 @@
-import people from './users.js'
-import {findUserById, updateUser as updateProfile} from "../../daos/users/users-dao.js";
-let users = people
+import * as userDao from "../../daos/users/users-dao.js";
 
 const UserController = (app) => {
-    app.get('/api/users', findUsers);
+    app.get('/api/users/:uid/excluded', findAllUsersWithoutMe);
     app.get('/api/users/:uid', findUserByUserId);
-    app.post('/api/users', createUser);
-    app.delete('/api/users/:uid', deleteUser);
     app.put('/api/users/:uid', updateUser);
 }
 
-const findUsers = (req, res) => {
-    const type = req.query.type
-    if(type) {
-        const usersOfType = users
-            .filter(u => u.type === type)
-        res.json(usersOfType)
-        return
-    }
-
-    res.json(users)
+const findAllUsersWithoutMe = async (req, res) => {
+    const userId = req.params.uid;
+    // filter out logged in user
+    const users = await userDao.findAllUsersWithoutMe(userId);
+    res.json(users);
 }
 
 const findUserByUserId = async (req, res) => {
     try {
         const userId = req.params.uid;
-        const user = await findUserById(userId)
+        const user = await userDao.findUserById(userId)
         res.json(user);
     } catch (err) {
         res.status(500).send(err.message);
     }
 }
 
-const createUser = (req, res) => {
-    const newUser = req.body;
-    newUser._id = (new Date()).getTime() + '';
-    users.push(newUser);
-    res.json(newUser);
-}
-
-const deleteUser = (req, res) => {
-    const userId = req.params['uid'];
-    users = users.filter(usr =>
-        usr._id !== userId);
-    res.sendStatus(200);
-}
-
-
 const updateUser = async (req, res) => {
     const userId = req.params['uid'];
     const updates = req.body;
-    await updateProfile(userId, updates)
+    console.log(updates);
+    await userDao.updateUser(userId, updates);
     res.sendStatus(200);
 }
 
